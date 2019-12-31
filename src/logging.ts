8aae587d87957e26b83483183eaef03601f57b67
Glitch (howsmydriving-twitter) <none>
2019-12-31 01:42:57 +0000

    Retries working.

--- src/logging.ts
+++ src/logging.ts
@@ -1,24 +1,57 @@
 import { sleep } from 'howsmydriving-utils';
 
-import { log4js_config_path } from '../index';
+import * as packpath from 'packpath';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { getAppRootPath } from './util/process';
+
+let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
+let packpath_self = packpath.self();
+
+let package_json_path = path.resolve(__dirname + '/../package.json');
+
+if (!fs.existsSync(package_json_path)) {
+  package_json_path = path.resolve(__dirname + '/../../package.json');
+
+  if (!fs.existsSync(package_json_path)) {
+    throw new Error(`Cannot find package.json: ${__dirname}.`);
+  }
+}
+
+console.log(`howsmydriving-twitter: Loading ${package_json_path}.`);
+var pjson = require(package_json_path);
+
+// Put this at very top so other modules can import it without taking
+// dependencies on something else in the module being instantiated.
+export const __MODULE_NAME__ = pjson.name;
+export const __MODULE_VERSION__ = pjson.version;
+
+const temp_log4js_config_path = path.resolve(
+  getAppRootPath() + '/dist/config/log4js.json'
+);
+
+if (!fs.existsSync(temp_log4js_config_path)) {
+  throw new Error(`Cannot find log4js.json: ${temp_log4js_config_path}.`);
+}
+
+export const log4js_config_path = temp_log4js_config_path;
 
 const chokidar = require('chokidar'),
-  log4js = require('log4js'),
-  path = require('path');
+  log4js = require('log4js');
 
 // Load the config.
 log4js.configure(log4js_config_path);
 
 // Create default logger to log that our module was loaded and for
 // config update changes.
-export var log = log4js.getLogger('result');
-
-import { __MODULE_NAME__, __MODULE_VERSION__ } from '../index';
-
-log.addContext('module', __MODULE_NAME__);
-log.info(
-  `howsmydriving-twitter: __MODULE_NAME__: ${__MODULE_NAME__}, __MODULE_VERSION__": ${__MODULE_VERSION__}.`
+var temp_log = log4js.getLogger('result');
+temp_log.addContext('module', __MODULE_NAME__);
+temp_log.info(
+  `howsmydriving-twitter: Adding log4js (${log4js_config_path}) context: ${__MODULE_NAME__}.`
 );
+
+export const log = temp_log;
 
 /**
  * Monitor the log4js config file and reloading log instances if the file changes.
